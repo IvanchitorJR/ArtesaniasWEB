dd59d5fe98b457334fa69a39753bfe482cce1a62
AlcachofaK_Dev <jonnyma1011@gmail.com>
2025-06-01 19:44:00 -0600

    Added Login
    Login agregado a la pagina

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.post('/login', (req, res) => {
-    const { usuario, contrasena } = req.body;
-    const sql = 'SELECT * FROM usuarios WHERE usuario = ? AND contrasena = ?';
-    db.query(sql, [usuario, contrasena], (err, result) => {
+    const { correo, contrasena } = req.body;
+    const sql = 'SELECT * FROM usuarios WHERE correo = ? AND Contrasena = ?';
+    db.query(sql, [correo, contrasena], (err, result) => {
         if (err) throw err;
         if (result.length > 0) {
             res.send('Login exitoso');
@@ -22,17 +22,17 @@ app.post('/login', (req, res) => {
 
 
 app.post('/signin', (req, res) => {
-    const { usuario, contrasena } = req.body;
+    const { correo, contrasena } = req.body;
     // Verifica si el usuario ya existe
-    const checkSql = 'SELECT * FROM usuarios WHERE usuario = ?';
-    db.query(checkSql, [usuario], (err, result) => {
+    const checkSql = 'SELECT * FROM usuarios WHERE correo = ?';
+    db.query(checkSql, [correo], (err, result) => {
         if (err) return res.send('Error en la base de datos');
         if (result.length > 0) {
             return res.send('El usuario ya existe');
         }
         // Inserta el nuevo usuario
-        const insertSql = 'INSERT INTO usuarios (usuario, contrasena) VALUES (?, ?)';
-        db.query(insertSql, [usuario, contrasena], (err2) => {
+        const insertSql = 'INSERT INTO usuarios (correo, contrasena) VALUES (?, ?)';
+        db.query(insertSql, [correo, contrasena], (err2) => {
             if (err2) return res.send('Error al crear la cuenta');
             res.send('Cuenta creada exitosamente');
         });
@@ -41,7 +41,7 @@ app.post('/signin', (req, res) => {
 
 
 
-app.listen(3000, () => {
-    console.log('Servidor corriendo en http://localhost:3000');
-    console.log('Pagina de login en http://localhost:3000/index.html');
+app.listen(4000, () => {
+    console.log('Servidor corriendo en http://localhost:4000');
+    console.log('Pagina de login en http://localhost:4000/index.html');
 });
